54ea629b65dc089f5acb8a0742a9ab7d8bd1a668
jimmy <jimmy29304825@gmail.com>
2020-03-22 17:33:10 +0800

    0322 update

--- controlPi/paramiko_test1.py
+++ controlPi/paramiko_test1.py
@@ -2,7 +2,8 @@ import paramiko
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect("192.168.0.37",22,"pi", "password")  # 網路連線設定
+ssh.connect("10.0.0.197",22,"pi", "Jimmy8193026")  # 網路連線設定
 stdin, stdout, stderr = ssh.exec_command("python /home/pi/Desktop/test.py 202002023g1")  # 呼叫執行檔並傳送參數
-print(stdout.readlines())  # 回傳產出結果
+paramiko_result = stdout.readlines()  # 回傳產出結果
+print(paramiko_result)
 ssh.close()
